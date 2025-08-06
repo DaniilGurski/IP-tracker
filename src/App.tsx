53936d980b19dd58c4b7d06261c290a055251a9e
Daniil Gurski <daniilgurski6@gmail.com>
2025-08-06 20:11:55 +0200

    add custom marker icon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Icon } from "leaflet";
+import iconLocation from "@assets/icon-location.svg";
 import { useAtomValue } from "jotai";
 import { ipAddressCoordsAtom } from "@/atoms";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import SetViewOnSubmit from "@components/SetViewOnSubmit";
 import IpTrackerContainer from "@components/IpTrackerContainer";
+
+const customMarkerIcon = new Icon({
+  iconUrl: iconLocation,
+  iconSize: [46, 56],
+});
 
 export default function App() {
   const ipAddressCoords = useAtomValue(ipAddressCoordsAtom);
@@ -15,7 +22,7 @@ export default function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={ipAddressCoords} />
+        <Marker position={ipAddressCoords} icon={customMarkerIcon} />
         <SetViewOnSubmit />
       </MapContainer>
     </main>
